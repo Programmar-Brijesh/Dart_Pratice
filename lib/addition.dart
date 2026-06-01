42a7729b10db0890e124f2f4bae5e66ebaa9ba11
Programmar-Brijesh <brijeshverma8896@gmail.com>
2026-06-01 09:03:44 +0530

    add comments in code

--- lib/addition.dart
+++ lib/addition.dart
@@ -1,10 +1,10 @@
-import 'dart:io';
+import 'dart:io'; // library package
 
 void main(){
-  stdout.write("Enter First Number : ");
-  int num1=int.parse(stdin.readLineSync()!);
-  stdout.write("Enter Second Number : ");
-  int num2=int.parse(stdin.readLineSync()!);
-  int sum=num1+num2;
-  stdout.write("the sum of $num1 + $num2 is $sum");
+  stdout.write("Enter First Number : "); // taking input from user
+  int num1=int.parse(stdin.readLineSync()!); // reading user input and storing in given variable
+  stdout.write("Enter Second Number : "); // taking input from user
+  int num2=int.parse(stdin.readLineSync()!); // reading user input and storing in given variable
+  int sum=num1+num2; // addition code 
+  stdout.write("the sum of $num1 + $num2 is $sum"); // print sum of given two number by user
 }
--- lib/first.dart
+++ lib/first.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+// hello  world program
+
 void main(){
   stdout.write("hello world");
   print("hello darkcypher");
